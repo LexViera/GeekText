a664ff6d85c6f0c809532d7d09dedd832d9da721
LexViera <alejandro.perez.viera@gmail.com>
2022-02-16 18:11:32 -0500

    update security configuration for MongoDb Atlas

--- Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/SecurityConfig.java
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/SecurityConfig.java
@@ -4,11 +4,16 @@ package group15.RestServicewMongoDB;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
+  
+    /*
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
       return http
@@ -17,5 +22,13 @@ public class SecurityConfig {
         .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
         .build();
       }
+      */
+    @Override
+      protected void configure(HttpSecurity http) throws Exception {
+           http.csrf().disable();
+           http.authorizeRequests().anyRequest().permitAll();
+    }
+
+    
   
   }
