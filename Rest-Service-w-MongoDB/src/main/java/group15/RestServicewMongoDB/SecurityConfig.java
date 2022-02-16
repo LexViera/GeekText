bff31944dbda256acabb059505775b157b81b64e
LexViera <alejandro.perez.viera@gmail.com>
2022-02-16 14:48:49 -0500

    Adding HTTPS Functionality and SSL Certificates to project

---
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/SecurityConfig.java
@@ -0,0 +1,21 @@
+package group15.RestServicewMongoDB;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+      return http
+        .requiresChannel(channel -> 
+            channel.anyRequest().requiresSecure())
+        //.authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+        .build();
+      }
+  
+  }
