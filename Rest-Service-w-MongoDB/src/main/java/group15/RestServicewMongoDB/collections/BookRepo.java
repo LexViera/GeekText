8d4d8f0e96e5cd8c914124f7d819d0f48cfa9b23
LexViera <alejandro.perez.viera@gmail.com>
2022-02-17 10:26:21 -0500

    File restructre

--- Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/collections/BookRepo.java
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/collections/BookRepo.java
@@ -1,6 +1,8 @@
-package group15.RestServicewMongoDB.models;
+package group15.RestServicewMongoDB.collections;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import group15.RestServicewMongoDB.models.Book;
 
 public interface BookRepo extends MongoRepository<Book, String> {
     
--- Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/collections/UserRepo.java
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/collections/UserRepo.java
@@ -1,6 +1,8 @@
-package group15.RestServicewMongoDB.models;
+package group15.RestServicewMongoDB.collections;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import group15.RestServicewMongoDB.models.User;
 
 public interface UserRepo extends MongoRepository<User, Integer> {
     
