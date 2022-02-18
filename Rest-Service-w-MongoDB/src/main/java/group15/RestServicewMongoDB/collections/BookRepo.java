df8de0d886b272ff6462c495ad3cded68421648e
Arturoo0 <arturoportelles@gmail.com>
2022-02-18 13:49:31 -0500

    Create a sessions mongo repo and refactor styling

--- Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/collections/BookRepo.java
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/collections/BookRepo.java
@@ -1,9 +1,6 @@
 package group15.RestServicewMongoDB.collections;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import group15.RestServicewMongoDB.models.Book;
 
-public interface BookRepo extends MongoRepository<Book, String> {
-    
-}
+public interface BookRepo extends MongoRepository<Book, String> {}
---
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/collections/SessionRepo.java
@@ -0,0 +1,6 @@
+package group15.RestServicewMongoDB.collections;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import group15.RestServicewMongoDB.models.Session;
+
+public interface SessionRepo extends MongoRepository<Session, String> {}
