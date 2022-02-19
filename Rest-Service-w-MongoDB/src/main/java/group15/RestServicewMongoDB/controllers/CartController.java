b8a73d030312cd611dbe4e18c12dfe983dff059a
jcpds <compaycito@gmail.com>
2022-02-18 21:58:54 -0500

    Added Shopping Cart model and CartController

---
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/controllers/CartController.java
@@ -0,0 +1,36 @@
+package group15.RestServicewMongoDB.controllers;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import group15.RestServicewMongoDB.models.Book;
+import group15.RestServicewMongoDB.models.ShoppingCart;
+
+@RestController
+public class CartController {
+
+    @GetMapping("/cart")
+    public List<Book> showCart(ShoppingCart cart)
+    {
+        return cart.getCart();
+    }
+    
+    @PostMapping("/add-to-cart")
+    public void addToCart(Book book, ShoppingCart cart)
+    {
+        cart.addBook(book);
+    }
+
+    @PostMapping("/remove-from-cart")
+    public void removeFromCart(Book book, ShoppingCart cart)
+    {
+        cart.removeBook(book);
+    }
+
+
+
+
+
+    
+}
---
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/models/ShoppingCart.java
@@ -0,0 +1,40 @@
+package group15.RestServicewMongoDB.models;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ShoppingCart {
+
+    //variables
+    private List<Book>cart = new LinkedList<>();
+
+    //empty constructor
+    public ShoppingCart(){}
+
+    //add book
+    public void addBook(Book book)
+    {
+        cart.add(book);
+    }
+
+    //remove book
+    public void removeBook(Book book)
+    {
+        for(Book x:cart)
+        {
+            if (book.getIsbn().equals(x.getIsbn()))
+            {
+                cart.remove(book);
+            }
+        }
+    }
+
+    //show cart
+    public List<Book> getCart() {
+        return cart;
+    }
+
+    
+
+
+    
+}
