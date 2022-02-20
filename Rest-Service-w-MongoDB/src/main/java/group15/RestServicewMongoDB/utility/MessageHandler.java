16e23c70ebd3f8c730b36529aa0c8c7960d1d5fa
Arturoo0 <41554978+Arturoo0@users.noreply.github.com>
2022-02-20 18:11:20 -0500

    Add back missing messages

--- Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/utility/MessageHandler.java
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/utility/MessageHandler.java
@@ -22,70 +22,72 @@ public class MessageHandler {
     }
 
     public static Message addedBooks(){
+        final String addedBooks = "Successfully added book(s)";
         return new Message(addedBooks, SUCCESS);
     }
 
     public static Message notAdmin(){
+        final String notAdmin = "Unauthorized";
         return new Message(notAdmin, ERROR);
     }
 
     public static Message failedToProvideCredentials(){
-        String failedToProvideCredentials =  "Failed to provide username and password credentials.";
+        final String failedToProvideCredentials =  "Failed to provide username and password credentials.";
         return new Message(failedToProvideCredentials, ERROR);
     }
 
     public static Message takenUser(){
-        String takenUser = "Entered username is taken.";
+        final String takenUser = "Entered username is taken.";
         return new Message(takenUser, ERROR);
     }
 
     public static Message createdAccount(){
-        String createdAccount = "Succesfully created account.";
+        final String createdAccount = "Succesfully created account.";
         return new Message(createdAccount, SUCCESS);
     }
 
     public static Message missingUser(){
-        String missingUser = "Username provided does not exist.";
+        final String missingUser = "Username provided does not exist.";
         return new Message(missingUser, ERROR);
     }
 
     public static Message passwordMismatch(){
-        String passwordMismatch = "Invalid login credentials provided.";
+        final String passwordMismatch = "Invalid login credentials provided.";
         return new Message(passwordMismatch, ERROR);
     }
 
     public static Message successfullySignedIn(){
-        String successfullySignedIn = "Successfully signed in.";
+        final String successfullySignedIn = "Successfully signed in.";
         return new Message(successfullySignedIn, SUCCESS);
     }
 
     public static Message notSignedIn(){
-        String notSignedIn = "You are not signed in.";
+        final String notSignedIn = "You are not signed in.";
         return new Message(notSignedIn, ERROR);
     }
 
     public static Message addedCreditCard(){
-        String addedCreditCard = "Succesfully added credit card.";
+        final String addedCreditCard = "Succesfully added credit card.";
         return new Message(addedCreditCard, SUCCESS);
     }
 
     public static Message maxAmountOfCards(){
-        String maxAmountOfCards = "You have hit the max credit card limit.";
+        final String maxAmountOfCards = "You have hit the max credit card limit.";
         return new Message(maxAmountOfCards, ERROR);
     }
 
     public static Message updatedUser(String userField){
-        String updatedUser = String.format("Succesfully updated user field '%s'", userField);
+        final String updatedUser = String.format("Succesfully updated user field '%s'", userField);
         return new Message(updatedUser, SUCCESS);
     }
 
     public static Message missingChangePasswordCredentials(){
-        String missingChangePasswordCredentials = "Must supply 'oldPassword' and 'newPassword' fields to change user password.";
+        final String missingChangePasswordCredentials = "Must supply 'oldPassword' and 'newPassword' fields to change user password.";
         return new Message(missingChangePasswordCredentials ,ERROR);
     }
 
     public static Message providedIncorrectOldPassword(){
-        String incorrectOldPassword = "Incorrect old password provided.";
+        final String incorrectOldPassword = "Incorrect old password provided.";
         return new Message(incorrectOldPassword, ERROR);
     }
 }
