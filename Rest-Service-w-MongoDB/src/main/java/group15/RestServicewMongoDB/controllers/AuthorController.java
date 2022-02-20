7965dd0e751702fe70e27d7f3c10b54f5cd58643
LexViera <alejandro.perez.viera@gmail.com>
2022-02-20 17:29:16 -0500

    Fix warnings

--- Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/controllers/AuthorController.java
+++ Rest-Service-w-MongoDB/src/main/java/group15/RestServicewMongoDB/controllers/AuthorController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
