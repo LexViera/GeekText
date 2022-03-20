25de5308bff6a1c2e6d550cfeb2e091b4f2c4cbd
LexViera <alejandro.perez.viera@gmail.com>
2022-03-20 08:10:00 -0400

    added new testing endpoint

--- src/main/java/group15/RestServicewMongoDB/controllers/BookController.java
+++ src/main/java/group15/RestServicewMongoDB/controllers/BookController.java
@@ -10,11 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import group15.RestServicewMongoDB.collections.BookRepo;
 import group15.RestServicewMongoDB.collections.SessionRepo;
@@ -45,6 +41,7 @@ public class BookController {
                    
         return MessageHandler.customSuccessMesssage("Hello World - Request Counter:"+ ++countr);
     }
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/test2")
     public Object test2(){          
         return MessageHandler.customSuccessMesssage("Hello World");
