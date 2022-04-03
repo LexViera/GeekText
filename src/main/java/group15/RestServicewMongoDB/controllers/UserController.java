cacf72f17be10a19fd1c31f75fe804fd09bc09fa
LexViera <alejandro.perez.viera@gmail.com>
2022-04-03 14:10:08 -0400

    CORS Policy mods

--- src/main/java/group15/RestServicewMongoDB/controllers/UserController.java
+++ src/main/java/group15/RestServicewMongoDB/controllers/UserController.java
@@ -74,7 +74,7 @@ public class UserController {
         }
         return signupUser(userCredentials, isAdmin);
     }
-    @CrossOrigin(origins = "https://geek-text-front.herokuapp.com/")
+    @CrossOrigin(origins = "https://geek-text-front.herokuapp.com/login")
     @PostMapping("/login")
     public Message loginUser(@RequestBody Login loginCredentials, HttpServletResponse response){  
         final String givenUsername, givenPassword;
