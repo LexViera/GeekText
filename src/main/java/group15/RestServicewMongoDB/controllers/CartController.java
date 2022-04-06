59359d611cddc4eb8ec197a3af864487c714726c
LexViera <alejandro.perez.viera@gmail.com>
2022-04-06 19:29:28 -0400

    target Session User on cart requests

--- src/main/java/group15/RestServicewMongoDB/controllers/CartController.java
+++ src/main/java/group15/RestServicewMongoDB/controllers/CartController.java
@@ -1,18 +1,31 @@
 package group15.RestServicewMongoDB.controllers;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import group15.RestServicewMongoDB.collections.SessionRepo;
+import group15.RestServicewMongoDB.collections.UserRepo;
 import group15.RestServicewMongoDB.models.Book;
 import group15.RestServicewMongoDB.models.User;
+import group15.RestServicewMongoDB.utility.SessionHandler;
 
 @RestController
 public class CartController {
 
+    @Autowired
+    private SessionRepo sessionCollection;
+    @Autowired
+    private UserRepo userCollection;
+    
     @GetMapping("/cart")
-    public List<Book> showCart(User user)
+    public List<Book> showCart(HttpServletRequest request)
     {
-        return user.getCart().getCartContent();
+        return SessionHandler.fetchRequestUser(request, sessionCollection, userCollection).getCart().getCartContent();
     }
     
     @PostMapping("/add-to-cart")
